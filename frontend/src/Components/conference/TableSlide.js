0175f94e1d28dcf036d614010bee8a16550b1196
youngchan choi <youngchan419@gmail.com>
2022-07-22 17:17:59 +0900

    chore: table customize slide 구현

---
+++ frontend/src/Components/conference/TableSlide.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Switch from '@mui/material/Switch';
+import Slide from '@mui/material/Slide';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import ItemTab from './ItemTab';
+
+function TableSlide() {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <div>
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleChange} />}
+          label="Show"
+        />
+        <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
+          <div>
+            <ItemTab></ItemTab>
+          </div>
+        </Slide>
+      </div>
+    </div>
+  );
+}
+
+export default TableSlide;
