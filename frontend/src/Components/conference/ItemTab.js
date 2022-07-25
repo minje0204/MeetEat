1019d5c4f986c542386c02054f1d33c39a646604
최영찬 <youngchan96@snu.ac.kr>
2022-07-25 14:34:23 +0900

    chore: add drag event function

--- frontend/src/Components/conference/ItemTab.js
+++ frontend/src/Components/conference/ItemTab.js
@@ -2,20 +2,19 @@ import { useState } from "react";
 import TableItems from "./TableItems";
 import styled from "styled-components";
 
-export default function ItemTab () {
-  
-  let posX = 0;
-  let posY = 0;
+export default function ItemTab() {
+  // let posX = 0;
+  // let posY = 0;
 
-  let originalX = 0;
-  let originalY = 0;
+  // let originalX = 0;
+  // let originalY = 0;
 
-  const [activeIndex, setActiveIndex]=useState(0);
-  
-  const tabClickHandler = (index) =>{
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const tabClickHandler = index => {
     setActiveIndex(index);
   };
-  
+
   // const dragStartHandler = e => {
   //   posX = e.clientX;
   //   posY = e.clientY;
@@ -32,27 +31,51 @@ export default function ItemTab () {
   //   console.log(posX, posY)
   // };
 
-  const tabContArr=[
+  const tabContArr = [
     {
-      tabTitle:(
-          <span className={activeIndex===0 ? "is-active" : ""} onClick={()=>tabClickHandler(0)}> 식기류 </span>
+      tabTitle: (
+        <span
+          className={activeIndex === 0 ? "is-active" : ""}
+          onClick={() => tabClickHandler(0)}
+        >
+          {" "}
+          식기류{" "}
+        </span>
       ),
     },
     {
-      tabTitle:(
-          <span className={activeIndex===1 ? "is-active" : ""} onClick={()=>tabClickHandler(1)}> 한식 </span>
+      tabTitle: (
+        <span
+          className={activeIndex === 1 ? "is-active" : ""}
+          onClick={() => tabClickHandler(1)}
+        >
+          {" "}
+          한식{" "}
+        </span>
       ),
     },
     {
-      tabTitle:(
-          <span className={activeIndex===2 ? "is-active" : ""} onClick={()=>tabClickHandler(2)}> 일식 </span>
+      tabTitle: (
+        <span
+          className={activeIndex === 2 ? "is-active" : ""}
+          onClick={() => tabClickHandler(2)}
+        >
+          {" "}
+          일식{" "}
+        </span>
       ),
     },
     {
-      tabTitle:(
-          <span className={activeIndex===3 ? "is-active" : ""} onClick={()=>tabClickHandler(3)}> 양식 </span>
+      tabTitle: (
+        <span
+          className={activeIndex === 3 ? "is-active" : ""}
+          onClick={() => tabClickHandler(3)}
+        >
+          {" "}
+          양식{" "}
+        </span>
       ),
-    }
+    },
   ];
 
   // const dragItem = document.querySelectorAll('span')
@@ -64,18 +87,18 @@ export default function ItemTab () {
     <StyledWrapper>
       <h2>식탁 꾸미기</h2>
       <div className="tabs is-boxed">
-        {tabContArr.map((section, index)=>{
-          return section.tabTitle
+        {tabContArr.map((section, index) => {
+          return section.tabTitle;
         })}
       </div>
       <TableItems tabSelect={activeIndex}></TableItems>
     </StyledWrapper>
-  )
-};
+  );
+}
 
 const StyledWrapper = styled.div`
-.is-active {
-  font-weight: bold;
-  text-decoration: underline;
-}
-`
+  .is-active {
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`;
--- frontend/src/Components/conference/TableItems.js
+++ frontend/src/Components/conference/TableItems.js
@@ -1,66 +1,136 @@
 import { useState } from "react";
 
-export default function TableItems (props) {
+export default function TableItems(props) {
+  const [tableDish, setTableDish] = useState([]);
+  const [targets, setTargets] = useState([]);
 
-  const [tableDish, setTableDish]=useState([]);
+  let posX = 0;
+  let posY = 0;
+
+  let originalX = 0;
+  let originalY = 0;
 
   const tableItems = [
-    {name: '숟가락', type:'silverware'},
-    {name: '젓가락', type:'silverware'},
-    {name: '김치', type:'kfood'},
-    {name: '된장찌개', type:'kfood'},
-    {name: '스시', type:'jfood'},
-    {name: '돈까스', type:'jfood'},
-    {name: '스테이크', type:'western'},
-    {name: '피자', type:'western'},
-  ]
+    { name: "숟가락", type: "silverware" },
+    { name: "젓가락", type: "silverware" },
+    { name: "김치", type: "kfood" },
+    { name: "된장찌개", type: "kfood" },
+    { name: "스시", type: "jfood" },
+    { name: "돈까스", type: "jfood" },
+    { name: "스테이크", type: "western" },
+    { name: "피자", type: "western" },
+  ];
 
-  
-  const tableDishHandler = (item) => {
-    setTableDish([...tableDish, item])
-  }
+  const tableDishHandler = item => {
+    setTableDish([...tableDish, item]);
+  };
 
-  
-  const rendering = (idx) => {
+  const dragStartHandler = e => {
+    posX = e.clientX;
+    posY = e.clientY;
+    originalX = e.target.offsetLeft;
+    originalY = e.target.offsetTop;
+  };
+
+  const dragHandler = e => {
+    e.target.style.left = `${e.target.offsetLeft + e.clientX - posX}px`;
+    e.target.style.top = `${e.target.offsetTop + e.clientY - posY}px`;
+    posY = e.clientY;
+    posX = e.clientX;
+  };
+
+  const dragEndHandler = e => {
+    setTargets(targets => {
+      const newTargets = [...targets];
+      newTargets.push({
+        id: parseInt(e.timeStamp),
+        top: e.target.offsetTop + e.clientY - posY,
+        left: e.target.offsetLeft + e.clientX - posX,
+        details: e.target.id,
+      });
+      console.log(newTargets);
+      return newTargets;
+    });
+
+    e.target.style.left = `${originalX}px`;
+    e.target.style.top = `${originalY}px`;
+  };
+
+  const rendering = idx => {
     const result = [];
-    if (idx===0) {
+    if (idx === 0) {
       for (let i = 0; i < tableItems.length; i++) {
-        if (tableItems[i].type==='silverware') {
-          result.push(<span key={i} onClick={()=>tableDishHandler(tableItems[i].name)} draggable='True'>{tableItems[i].name}</span>)
-          result.push(<br></br>)
+        if (tableItems[i].type === "silverware") {
+          result.push(
+            <span
+              key={i}
+              id={tableItems[i].name}
+              onClick={() => tableDishHandler(tableItems[i].name)}
+              draggable="True"
+              onDragStart={dragStartHandler}
+              onDrag={dragHandler}
+              onDragEnd={dragEndHandler}
+            >
+              {tableItems[i].name}
+            </span>,
+          );
+          result.push(<br></br>);
         }
       }
-    } else if (idx===1) {
+    } else if (idx === 1) {
       for (let i = 0; i < tableItems.length; i++) {
-        if (tableItems[i].type==='kfood') {
-          result.push(<span key={i} onClick={()=>tableDishHandler(tableItems[i].name)} draggable='True'>{tableItems[i].name}</span>)
-          result.push(<br></br>)
+        if (tableItems[i].type === "kfood") {
+          result.push(
+            <span
+              key={i}
+              onClick={() => tableDishHandler(tableItems[i].name)}
+              draggable="True"
+            >
+              {tableItems[i].name}
+            </span>,
+          );
+          result.push(<br></br>);
         }
-      }  
-    } else if (idx===2) {
+      }
+    } else if (idx === 2) {
       for (let i = 0; i < tableItems.length; i++) {
-        if (tableItems[i].type==='jfood') {
-          result.push(<span key={i} onClick={()=>tableDishHandler(tableItems[i].name)} draggable='True'>{tableItems[i].name}</span>)
-          result.push(<br></br>)
+        if (tableItems[i].type === "jfood") {
+          result.push(
+            <span
+              key={i}
+              onClick={() => tableDishHandler(tableItems[i].name)}
+              draggable="True"
+            >
+              {tableItems[i].name}
+            </span>,
+          );
+          result.push(<br></br>);
         }
-      }  
-    } else if (idx===3) {
+      }
+    } else if (idx === 3) {
       for (let i = 0; i < tableItems.length; i++) {
-        if (tableItems[i].type==='western') {
-          result.push(<span key={i} onClick={()=>tableDishHandler(tableItems[i].name)} draggable='True'>{tableItems[i].name}</span>)
-          result.push(<br></br>)
+        if (tableItems[i].type === "western") {
+          result.push(
+            <span
+              key={i}
+              onClick={() => tableDishHandler(tableItems[i].name)}
+              draggable="True"
+            >
+              {tableItems[i].name}
+            </span>,
+          );
+          result.push(<br></br>);
         }
       }
     }
 
     return result;
-
-  }
+  };
 
   return (
     <div id="table-items">
       {rendering(props.tabSelect)}
       선택된 것들 : {tableDish}
     </div>
-  )
+  );
 }
