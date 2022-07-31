c6940f01d659b61f92b3748c795fe1a92e5d315e
minje0204 <minje@ajou.ac.kr>
2022-07-31 18:49:43 +0900

    feat: 화상회의 페이지 테이블 렌더링 이슈 해결

--- frontend/src/views/conference/ConferencePage.js
+++ frontend/src/views/conference/ConferencePage.js
@@ -1,9 +1,10 @@
-// import styled from "@emotion/styled";
-import RoomGuests from "components/conference/RoomGuests";
+import styled from "@emotion/styled";
+import RoomGuest from "components/conference/RoomGuest";
 import TableSlide from "components/conference/TableSlide";
 import UseSocket from "hooks/UseSocket";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import _ from "lodash";
 
 function Conference() {
   const location = useLocation();
@@ -20,15 +21,44 @@ function Conference() {
       room: title,
     };
     handleClickSendMessage(message);
-  }, [title, name]);
+  }, [title, name, handleClickSendMessage]);
+
+  const roomGuestList = (
+    <div id="room_guest_row">
+      {_.range(0, people).map(() => (
+        <RoomGuest />
+      ))}
+    </div>
+  );
+
   return (
-    <div>
+    <StyledWrapper>
       {/* <h1>
         컨퍼런스{params.conf_id} - {title} (현재인원수/{people}명)
       </h1> */}
       <TableSlide></TableSlide>
-      <RoomGuests people={people}></RoomGuests>
-    </div>
+      {roomGuestList}
+    </StyledWrapper>
   );
 }
 export default Conference;
+const StyledWrapper = styled.div`
+  #room_guest_row {
+    height: 90vh;
+    width: 100vw;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    align-content: center;
+  }
+  #room_guest_row_4 {
+    height: 90vh;
+    margin: 0 10vw;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    align-content: center;
+  }
+`;
