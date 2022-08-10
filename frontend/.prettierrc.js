1ac6e0b0793e75077b1cbe37c81012586a096a44
youngchan choi <youngchan419@gmail.com>
2022-08-10 12:17:10 +0900

    fix: rename directory name with lowercase, delete cached files

--- frontend/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: "all",
-  printWidth: 80,
-  semi: true,
-  arrowParens: "avoid",
-}
--- frontend/src/utils/axios/Axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const Axios = axios.create({
-  baseURL: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_URL}`,
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
-});
-
-export default Axios;
