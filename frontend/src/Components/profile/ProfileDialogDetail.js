f1040ecd21e3c5b86988f12caf63d3c0f5778d3c
dozinq <dojinuk@kakao.com>
2022-08-10 11:42:15 +0900

    -

--- frontend/src/Components/profile/ProfileDialogDetail.js
+++ frontend/src/Components/profile/ProfileDialogDetail.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import closebutton from "assets/img/closebutton.png";
+import Modal from '@mui/material/Modal';
 
 export default function ProfileDialogDetail(props) {
   const { open, onClose } = props;
-  
+    
   const handleClose = () => {
     onClose();
   };
@@ -13,7 +14,7 @@ export default function ProfileDialogDetail(props) {
   return (
     
     <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
+      <DialogTitle></DialogTitle>
       <button onClick={handleClose}>창닫기</button>
       <img src={ closebutton } id="exit-icon" alt="창닫기" onClick={handleClose}/>
     </Dialog>
