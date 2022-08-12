471cd5618e2b4f148121f28d2d7da97f82826294
minje0204 <minje@ajou.ac.kr>
2022-08-12 16:20:44 +0900

    temp: change allowed origin *

--- backend/src/main/java/com/a105/config/WebMvcConfig.java
+++ backend/src/main/java/com/a105/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.a105.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,8 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
-     * cors 설정
-     * preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간 : 60분
+     * cors 설정 preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간 : 60분
      */
     private final long MAX_AGE_SECS = 3600;
 
@@ -21,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(allowedOrigins)
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
