a0d00557b479e502bbc63cdc9386375959e10b49
민상규 <qksl7504@naver.com>
2022-08-16 00:19:36 +0900

    refactor : conferenceAPI

--- backend/src/main/java/com/a105/api/controller/ConferenceController.java
+++ backend/src/main/java/com/a105/api/controller/ConferenceController.java
@@ -6,12 +6,15 @@ import com.a105.api.request.ConferenceRequest;
 import com.a105.api.response.ConferenceListResponse;
 import com.a105.api.response.DefaultResponse;
 import com.a105.api.response.ResponseCode;
+import com.a105.api.response.UserInfoResponse;
+import com.a105.api.service.AuthService;
 import com.a105.api.service.ConferenceService;
 import com.a105.api.service.UserConferenceService;
 import com.a105.domain.conference.Conference;
 import com.a105.security.CurrentUser;
 import com.a105.security.UserPrincipal;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +26,7 @@ public class ConferenceController {
 
     private final ConferenceService conferenceService;
     private final UserConferenceService userConferenceService;
+    private final AuthService authService;
 
     @GetMapping("/{restaurantId}")
     private ResponseEntity<?> getConferenceList(
@@ -32,24 +36,36 @@ public class ConferenceController {
     }
 
     @GetMapping("/{restaurantId}/conference/{conferenceId}")
-    private ResponseEntity<?> joinConference(@CurrentUser UserPrincipal userPrincipal,
+    private ResponseEntity<?> joinConference(HttpServletRequest request,
         @PathVariable Long conferenceId) {
-        Conference conference = conferenceService.joinConference(conferenceId, userPrincipal.getId());
-        return ResponseEntity.ok().body(DefaultResponse.of(ResponseCode.OK, JOIN_CONFERENCE, conference));
+        String headerValue = request.getHeader("Authorization");
+        String token = headerValue.substring("Bearer ".length());
+        UserInfoResponse userInfoResponse = authService.getUserInfoFromToken(token);
+        Conference conference = conferenceService.joinConference(conferenceId,
+            userInfoResponse.getId());
+        return ResponseEntity.ok()
+            .body(DefaultResponse.of(ResponseCode.OK, JOIN_CONFERENCE, conference));
     }
 
     @PostMapping("/{restaurantId}")
-    private ResponseEntity<?> createConference(@CurrentUser UserPrincipal userPrincipal,
+    private ResponseEntity<?> createConference(HttpServletRequest request,
         @PathVariable int restaurantId, @RequestBody ConferenceRequest conferenceRequest) {
+        String headerValue = request.getHeader("Authorization");
+        String token = headerValue.substring("Bearer ".length());
+        UserInfoResponse userInfoResponse = authService.getUserInfoFromToken(token);
         Conference conference = conferenceService.createConference(conferenceRequest,
-            userPrincipal.getId(), restaurantId);
-        return ResponseEntity.ok().body(DefaultResponse.of(ResponseCode.OK, CREATE_CONFERENCE, conference));
+            userInfoResponse.getId(), restaurantId);
+        return ResponseEntity.ok()
+            .body(DefaultResponse.of(ResponseCode.OK, CREATE_CONFERENCE, conference));
     }
 
     @PatchMapping("/{restaurantId}/conference/{conferenceId}")
-    private ResponseEntity<?> leaveConference(@CurrentUser UserPrincipal userPrincipal,
+    private ResponseEntity<?> leaveConference(HttpServletRequest request,
         @PathVariable Long conferenceId) {
-        userConferenceService.leaveConference(conferenceId, userPrincipal.getId());
+        String headerValue = request.getHeader("Authorization");
+        String token = headerValue.substring("Bearer ".length());
+        UserInfoResponse userInfoResponse = authService.getUserInfoFromToken(token);
+        userConferenceService.leaveConference(conferenceId, userInfoResponse.getId());
         return ResponseEntity.ok().body(DefaultResponse.of(ResponseCode.OK, LEAVE_CONFERENCE));
     }
 }
